df1492ee838cad2d609b6a5e594959c3048d61ae
Juan Camilo Sandoval Devia <juansandoval@rccl.com>
2017-11-23 18:55:13 -0500

    dev_1.0.0-1 -MVP Started for construction. -Model for Posts created.

---
+++ app/src/main/java/com/mypostprodigious/juansandoval/mypost_prodigious/MVP/Presenter/MainScreenPresenter.java
@@ -0,0 +1,5 @@
+package com.mypostprodigious.juansandoval.mypost_prodigious.MVP.Presenter;
+
+
+public class MainScreenPresenter {
+}
---
+++ app/src/main/java/com/mypostprodigious/juansandoval/mypost_prodigious/MVP/View/MainScreenView.java
@@ -0,0 +1,5 @@
+package com.mypostprodigious.juansandoval.mypost_prodigious.MVP.View;
+
+
+public interface MainScreenView {
+}
